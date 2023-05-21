fc2b74d3af271947efda8b5c478a8b52a6a2e8b1
Brian Wong <brianw7218@gmail.com>
2023-05-20 22:49:40 -0700

    Create a nav bar for easier site navigation

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", { user: req.user });
 });
 
 module.exports = app;
