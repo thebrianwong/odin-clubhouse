2d8a4337025dfc72f8fc8b741312dfa8d8c01cdf
Brian Wong <brianw7218@gmail.com>
2023-05-21 16:01:14 -0700

    Redirect to posts from the landing page if already logged in

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  if (req.user) {
+    res.redirect("/post");
+    return;
+  }
   res.render("index");
 });
 
